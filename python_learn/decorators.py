96a0e296162b7a656e1f4bafc24361d66de640db
Xinlue Liu <42654960+XinlueLiu@users.noreply.github.com>
2022-08-10 11:45:26 -0700

    decorators

---
+++ python_learn/decorators.py
@@ -0,0 +1,21 @@
+# decorator function to convert to lowercase
+def lowercase_decorator(function):
+   def wrapper():
+       func = function()
+       string_lowercase = func.lower()
+       return string_lowercase
+   return wrapper
+# decorator function to split words
+def splitter_decorator(function):
+   def wrapper():
+       func = function()
+       string_split = func.split()
+       return string_split
+   return wrapper
+# can just comment out @decorators if no longer need decorator functions
+@splitter_decorator # this is executed next
+@lowercase_decorator # this is executed first
+def hello():
+   return 'Hello World'
+greet_message = hello()   # output => [ 'hello' , 'world' ]
+print(greet_message)
